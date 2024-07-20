712fa9d550e1cf302b997264a2446e62d10bac11
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-20 13:06:32 -0300

    Initial Commit

---
+++ TablesGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TablesGame
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 20/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TablesGame/TablesGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesGameApp.swift
+//  TablesGame
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 20/07/24.
+//
+
+import SwiftUI
+
+@main
+struct TablesGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
